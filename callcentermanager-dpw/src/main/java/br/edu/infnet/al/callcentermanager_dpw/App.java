dc68cfe7c7eba017289bae7ce1b2b766f391c130
PJ <PJ@PJ>
2021-04-02 14:37:42 -0300

    COMMIT INICIAL
    Detalhes no cabeçalho do App.java
    
    TO DO: Readme.md

---
+++ callcentermanager-dpw/src/main/java/br/edu/infnet/al/callcentermanager_dpw/App.java
@@ -0,0 +1,22 @@
+package br.edu.infnet.al.callcentermanager_dpw;
+
+/**
+ * Instituto InfNet
+ * 
+ * Disciplina: Tecnologia Java 2021, 1º semestre
+ * 
+ * Professor: Ed. Veloso
+ * 
+ * Integrantes do grupo: Daniel Barroso, Paulo Gurjão, William Pereira
+ * 
+ * Prazo de entrega: 12/04/2021 - segunda-feira
+ * 
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Call Center Manager DPW v0.1" );
+        System.out.println( "(Daniel, Paulo e William" );
+    }
+}
