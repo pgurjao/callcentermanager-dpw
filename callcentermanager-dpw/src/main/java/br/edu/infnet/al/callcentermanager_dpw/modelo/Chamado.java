6e52b4d451d5ddd569fe0d564bec2d6d923c3101
PJ <PJ@PJ>
2021-04-07 20:25:43 -0300

    Criada classe Enums para armazenar os enums "status" e "resultado".
    Alterados atributos da classe Chamado para refletir a mudança acima.

--- callcentermanager-dpw/src/main/java/br/edu/infnet/al/callcentermanager_dpw/modelo/Chamado.java
+++ callcentermanager-dpw/src/main/java/br/edu/infnet/al/callcentermanager_dpw/modelo/Chamado.java
@@ -5,23 +5,47 @@ public class Chamado {
 	private int idChamado;
 	private Cliente cliente;
 //	private int idCliente; // Colocar o objeto Cliente ou apenas o idCliente associado ao chamado?
-	private int idProduto;
-//	private Produto produto; // Colocar o objeto Produto ou apenas o idProduto associado ao chamado?
+	private Produto produto; 
+//	private int idProduto; // Colocar o objeto Produto ou apenas o idProduto associado ao chamado?
 	private int numeroSerieProduto; //Colocar numero de serie do produto no chamado ou deixar pra pegar direto do objeto Produto? 
 	private String descricaoResumida;
 	private String detalhamentoCompleto;
 	private int codigoErro;
-	private enum Status {
-						CADASTRADO,
-						EM_ATENDIMENTO,
-						FINALIZADO
-						}
-	private enum Resultado {
-							CANCELADO,
-							CONSERTADO,
-							SEM_SOLUCAO
-							}
+	private String status;
+	private String resultado;
 	
-	
+	public Chamado(Cliente cliente, Produto produto, String descricaoResumida) {
+		this.cliente = cliente;
+		this.produto = produto;
+		this.descricaoResumida = descricaoResumida;
+	}
+
+	public int getIdChamado() {
+		return idChamado;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public int getNumeroSerieProduto() {
+		return numeroSerieProduto;
+	}
+
+	public String getDescricaoResumida() {
+		return descricaoResumida;
+	}
+
+	public String getDetalhamentoCompleto() {
+		return detalhamentoCompleto;
+	}
+
+	public int getCodigoErro() {
+		return codigoErro;
+	}
 	
 }
---
+++ callcentermanager-dpw/src/main/java/br/edu/infnet/al/callcentermanager_dpw/modelo/Enums.java
@@ -0,0 +1,16 @@
+package br.edu.infnet.al.callcentermanager_dpw.modelo;
+
+public class Enums {
+
+	public static enum status {
+		CADASTRADO,
+		EM_ATENDIMENTO,
+		FINALIZADO
+	}
+	
+	public static enum resultado {
+		CANCELADO,
+		CONSERTADO,
+		SEM_SOLUCAO
+	}
+}
